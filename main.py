9448d49ca412e3df4d9d295bb3862c27fe75311a
Vanda Farsad <v.farsad@orendtstudios.com>
2023-06-07 21:23:03 +0200

    add log file

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ import logging
 
 from helpers import read_env_file
 from jobs.collect_feeds import CollectFeedsJob
+from settings import LOGS_PATH
 
-logger = logging.getLogger("GetFeedJob")
+
+LOGS_PATH.mkdir(exist_ok=True)
+
+logger = logging.getLogger(__name__)
+
+stdout_handler = logging.StreamHandler()
+stdout_handler.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler(LOGS_PATH / "logs.log")
+file_handler.setLevel(logging.WARNING)
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[logging.StreamHandler()],
+    handlers=[stdout_handler, file_handler],
 )
 
 
--- settings.py
+++ settings.py
@@ -2,4 +2,7 @@ from pathlib import Path
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 FEED_EXPORT_PATH = BASE_DIR / "feed_export"
+
+LOGS_PATH = BASE_DIR / "logs"
